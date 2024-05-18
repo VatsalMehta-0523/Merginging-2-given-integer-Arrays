119c19ed7c81c2c3942cf5b35f49f38cff8afa0c
VatsalMehta-0523 <168580559+VatsalMehta-0523@users.noreply.github.com>
2024-05-18 21:45:16 +0530

    merging of 2 integer arrays.c

--- merging of 2 integer arrays.c
+++ merging of 2 integer arrays.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
-void printArr( int arr[] , int n );
-void merge_arrays(int arr1[], int arr2[], int n , int m );
+void printArr( int arr[] , int n );                                   // function declaration.
+void merge_arrays(int arr1[], int arr2[], int n , int m );             
 
 int main(){
-    int n , m ;
+    int n , m ;                                                       // n & m is sizes of array1 & array2 respectively
     printf("enter size of array1 : ");
     scanf("%d", &n);
     int arr1[n];
-    printf("enter values of array 1: ");
+    printf("enter values of array 1: ");                              // run time array initialising...
     for (int i = 0 ; i < n ; i++ ){
         scanf("%d", &arr1[i]);
     }
@@ -20,7 +20,7 @@ int main(){
     }
 
     printf("merged array is : ");
-    merge_arrays(arr1 , arr2 , n , m );
+    merge_arrays(arr1 , arr2 , n , m );                            // function revoke / function call
 
 
     return 0 ;
@@ -28,7 +28,7 @@ int main(){
 
 
 
-void printArr( int arr[] , int n ){  // function to print array
+void printArr( int arr[] , int n ){                                 // function to print array
     for (int i = 0 ; i < n ; i++){
         printf("%d ", arr[i]);
     }
@@ -36,14 +36,14 @@ void printArr( int arr[] , int n ){  // function to print array
 
 
 
-void merge_arrays(int arr1[], int arr2[], int n , int m ){
-    int mer_arr[n+m];
-    for (int i = 0 ; i < n ;  i++ ){
+void merge_arrays(int arr1[], int arr2[], int n , int m ){             // function defination
+    int mer_arr[n+m];                                                 
+    for (int i = 0 ; i < n ;  i++ ){                                   // here elements from array1 is inserted from 0th index to (n-1)th index
             mer_arr[i] = arr1[i] ;
     }
-    for (int i = n , j = 0 ; i < n+m , j < m ; i++ , j++ ){
-            mer_arr[i] = arr2[j] ;
+    for (int i = n , j = 0 ; i < n+m , j < m ; i++ , j++ ){            
+            mer_arr[i] = arr2[j] ;                                     // here elements from array2 is inserted from nth index to (m-1)th  
     }
 
-    printArr(mer_arr , n+m );  // calling other funtion in defination
+    printArr(mer_arr , n+m );                                           // calling other funtion in defination
 }
